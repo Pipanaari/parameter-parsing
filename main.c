c4297599eb08e40fa6cdcb2351737fe192519c90
Pipanaari <109987161+Pipanaari@users.noreply.github.com>
2026-04-16 21:49:55 +0300

    finish error message

--- main.c
+++ main.c
@@ -7,14 +7,20 @@ int main(int arg_count, char **argv){
 		printf("input \"add\" or \"status\"");
 		return 1;
 	}
-
+	int change = 0;
 	for(int i = 1; i < arg_count; i++){
 		if(strcmp(argv[i], "add") == 0){
 			printf("added\n");
+			change = 1;
 		}
 		if(strcmp(argv[i], "status") == 0){
 			printf("status\n");
+			change = 1;
 		}
+	}
+	if (change == 0){
+		printf("input \"add\" or \"status\"");
+		return 1;
 	}
 	return 0;
 }
